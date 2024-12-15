3240ecf1813d3978a9035ca5f5ca3303a8894198
magdaPulter <magda.pulter@gmail.com>
2024-12-15 16:50:06 +0100

    firebase config updated

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: 'AIzaSyCJQxnK68ljNnPOCG6DL1MFtpDUJdcZ1FM',
-    authDomain: 'production-menagment-app.firebaseapp.com',
-    projectId: 'production-menagment-app',
-    storageBucket: 'production-menagment-app.firebasestorage.app',
-    messagingSenderId: '477685858858',
-    appId: '1:477685858858:web:46ff6bef7f292770623953',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
   },
 };
