96828a681876f57f391a068491b553b5ed78ad77
magdaPulter <magda.pulter@gmail.com>
2024-12-15 19:34:50 +0100

    create, delete, update methods implemented

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { map, Observable } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { InventoryModel } from '../models/inventory.model';
 
 @Injectable({
@@ -13,5 +13,46 @@ export class InventoryService {
     return this.firestore
       .collection<InventoryModel>('inventory')
       .valueChanges({ idField: 'id' });
+  }
+
+  createInventory(inventory: InventoryModel): Observable<string> {
+    const id = this.firestore.createId();
+    this.firestore
+      .collection('inventory')
+      .doc(id)
+      .set({ ...inventory, id });
+    return of(id);
+  }
+
+  deleteInventory(id: string): Observable<InventoryModel> {
+    return new Observable((observer) => {
+      this.firestore
+        .collection('inventory')
+        .doc(id)
+        .delete()
+        .then(() => {
+          observer.next();
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
+  }
+
+  updateInventory(inventory: InventoryModel): Observable<void> {
+    return new Observable((observer) => {
+      this.firestore
+        .collection('inventory')
+        .doc(inventory.id)
+        .update({ ...inventory })
+        .then(() => {
+          observer.next();
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
   }
 }
