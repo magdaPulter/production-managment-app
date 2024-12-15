fa1d478d1eb5d3ecd830eb2e5969b4ac6cc4fba8
magdaPulter <magda.pulter@gmail.com>
2024-12-15 15:28:46 +0100

    app created, firestore set up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environment/environment.prod';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      AngularFirestoreModule,
+      AngularFireModule.initializeApp(environment.firebaseConfig),
+    ]),
+  ],
 };
