03747088bad4ed276876e6b2f8f3f37980e989eb
magdaPulter <magda.pulter@gmail.com>
2024-12-28 11:52:45 +0100

    orderlist component completed

--- src/app/components/orders-list/orders-list.component.ts
+++ src/app/components/orders-list/orders-list.component.ts
@@ -1,15 +1,41 @@
-import { Component, Input, signal } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { OrderModel } from '../../models/order.model';
+import { ListItemViewModel } from '../../viewModels/listItem.viewModel';
+import { OrderFormComponent } from '../order-form/order-form.component';
 
 @Component({
   selector: 'app-orders-list',
   standalone: true,
-  imports: [MatExpansionModule],
+  imports: [MatExpansionModule, OrderFormComponent],
   templateUrl: './orders-list.component.html',
   styleUrl: './orders-list.component.scss',
 })
 export class OrdersListComponent {
   readonly panelOpenState = signal(false);
-  @Input() listItems!: OrderModel[];
+  readonly elementSelectedToEdit: WritableSignal<ListItemViewModel | null> =
+    signal(null);
+  @Input() listItems!: ListItemViewModel[];
+  @Output() listElementDeleted: EventEmitter<ListItemViewModel> =
+    new EventEmitter<ListItemViewModel>();
+
+  @Output() listElementEdited: EventEmitter<ListItemViewModel> =
+    new EventEmitter<ListItemViewModel>();
+
+  onElementRemoved(element: ListItemViewModel) {
+    this.listElementDeleted.emit(element);
+  }
+  onElementEditted(element: ListItemViewModel) {
+    this.elementSelectedToEdit.set(element);
+  }
+  onCloseEditForm(element: ListItemViewModel) {
+    this.listElementEdited.emit(element);
+    this.elementSelectedToEdit.set(null);
+  }
 }
