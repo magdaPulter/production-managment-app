0dcac716ed5ac14c05836e187367a4f94a1c227b
magdaPulter <magda.pulter@gmail.com>
2024-12-29 18:57:55 +0100

    login page implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { InventoryPageComponent } from './components/inventory-page/inventory-page.component';
 import { OrdersPageComponent } from './components/orders-page/orders-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'orders-page',
     component: OrdersPageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
   },
 ];
