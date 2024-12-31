23ff28d1273c7905ff982bd1ceca58073aa34f99
magdaPulter <magda.pulter@gmail.com>
2024-12-31 17:02:18 +0100

    loginpage removed from route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { InventoryPageComponent } from './components/inventory-page/inventory-page.component';
 import { OrdersPageComponent } from './components/orders-page/orders-page.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
 
 export const routes: Routes = [
   {
@@ -11,9 +10,5 @@ export const routes: Routes = [
   {
     path: 'orders-page',
     component: OrdersPageComponent,
-  },
-  {
-    path: 'login',
-    component: LoginPageComponent,
   },
 ];
