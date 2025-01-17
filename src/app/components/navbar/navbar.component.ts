cc7344fb7d4e73f12b2692481208d5e729dbf6e9
magdaPulter <magda.pulter@gmail.com>
2025-01-17 08:52:31 +0100

    mobile view to navbar added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Output,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,11 +15,27 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
+  animations: [
+    trigger('openClose', [
+      transition(':enter', [
+        style({ transform: 'translateX(120%)' }),
+        animate('1s ease-in', style({ transform: 'translateX(0%)' })),
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(0%)' }),
+        animate('1s ease-out', style({ transform: 'translateX(120%)' })),
+      ]),
+    ]),
+  ],
 })
 export class NavbarComponent {
   @Output() logOutEmitted: EventEmitter<void> = new EventEmitter<void>();
-
+  readonly isMenuOpen: WritableSignal<boolean> = signal(false);
   logOut() {
     this.logOutEmitted.emit();
+  }
+
+  onButtonclicked() {
+    this.isMenuOpen.update((isOpen) => !isOpen);
   }
 }
