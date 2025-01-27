0147ec305638675927153ba1dedf6efeee8adf4f
magdaPulter <magda.pulter@gmail.com>
2025-01-27 16:48:54 +0100

    card-item component updated

--- src/app/components/card-item/card-item.component.ts
+++ src/app/components/card-item/card-item.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { StockViewModel } from '../../viewModels/stock.viewModel';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-card-item',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, FormsModule],
   templateUrl: './card-item.component.html',
   styleUrl: './card-item.component.scss',
 })
 export class CardItemComponent {
   @Input() item!: StockViewModel;
+  roasted: number = 0;
 }
