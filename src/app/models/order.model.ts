445ca629662025a5684861c48094ad96df310a74
magdaPulter <magda.pulter@gmail.com>
2025-01-31 16:32:57 +0100

    product property added

--- src/app/models/order.model.ts
+++ src/app/models/order.model.ts
@@ -1,5 +1,8 @@
+import { ProductModel } from './product.model';
+
 export interface OrderModel {
   readonly id?: string;
   readonly name: string;
   readonly priority: string;
+  readonly products: ProductModel[];
 }
